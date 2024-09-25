834f7c247d14fbfb1af6d52d30b1f96e606aa132
Adedokun Adeshina <adeshina93@gmail.com>
2024-09-25 13:25:48 -0700

    update

---
+++ src/css/js/script.js
@@ -0,0 +1,14 @@
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDTkXjNDPdbOeo32CaCkglIy-6nzuszEJM",
+      authDomain: "orion-investment-2b141.firebaseapp.com",
+      projectId: "orion-investment-2b141",
+      storageBucket: "orion-investment-2b141.appspot.com",
+      messagingSenderId: "911866094162",
+      appId: "1:911866094162:web:bfe2dc2323995f4585358b",
+      measurementId: "G-12SQKBDR1S"
+    };
+
+
+    const app = firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
