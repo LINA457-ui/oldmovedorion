406465172d2506006c8dccb10f5746b2f35530b4
Adedokun Adeshina <adeshina93@gmail.com>
2025-01-31 23:26:19 -0800

    pushh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_MEASUREMENT_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDTkXjNDPdbOeo32CaCkglIy-6nzuszEJM",
+  authDomain: "orion-investment-2b141.firebaseapp.com",
+  projectId: 'orion-investment-2b141',
+  storageBucket: "orion-investment-2b141.appspot.com",
+  messagingSenderId: "911866094162",
+  appId: "1:911866094162:web:bfe2dc2323995f4585358b",
+  measurementId: "G-12SQKBDR1S"
 };
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
+
+
+
 
 
 
