4dcd9bed1e8338eb79564ce3e842d48a250824c3
Adedokun Adeshina <adeshina93@gmail.com>
2025-02-05 06:41:41 -0800

    done fine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,32 @@
-// Import Firebase modules correctly
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDTkXjNDPdbOeo32CaCkglIy-6nzuszEJM",
-  authDomain: "orion-investment-2b141.firebaseapp.com",
-  projectId: "orion-investment-2b141",
-  storageBucket: "orion-investment-2b141.appspot.com",
-  messagingSenderId: "911866094162",
-  appId: "1:911866094162:web:bfe2dc2323995f4585358b",
-  measurementId: "G-12SQKBDR1S",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Initialize services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
+// Initialize Firebase App
+const app = firebase.initializeApp(firebaseConfig);
 
-export { app, auth, db, storage };
+// Initialize Firebase Services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTkXjNDPdbOeo32CaCkglIy-6nzuszEJM",
+//   authDomain: "orion-investment-2b141.firebaseapp.com",
+//   projectId: "orion-investment-2b141",
+//   storageBucket: "orion-investment-2b141.appspot.com",
+//   messagingSenderId: "911866094162",
+//   appId: "1:911866094162:web:bfe2dc2323995f4585358b",
+//   measurementId: "G-12SQKBDR1S",
+// };
