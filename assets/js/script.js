b542d04cd48d743d484a24e0efeb079be63466b1
Lina <linapadovano611@gmail.com>
2025-06-05 10:29:48 -0700

    lina switch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDTkXjNDPdbOeo32CaCkglIy-6nzuszEJM",
-  authDomain: "orion-investment-2b141.firebaseapp.com",
-  projectId: "orion-investment-2b141",
-  storageBucket: "orion-investment-2b141.appspot.com",
-  messagingSenderId: "911866094162",
-  appId: "1:911866094162:web:bfe2dc2323995f4585358b",
-  measurementId: "G-12SQKBDR1S",
+  apiKey: "AIzaSyA8llOf1FwcwHWETusV9T6DphCIBAFd9D0",
+  authDomain: "investmentorion.firebaseapp.com",
+  projectId: "investmentorion",
+  storageBucket: "investmentorion.firebasestorage.app",
+  messagingSenderId: "914571699893",
+  appId: "1:914571699893:web:6f97d2a547125d736900fc",
 };
 
 // Initialize Firebase App
@@ -18,3 +17,14 @@ const storage = firebase.storage();
 
 
 
+
+
+
+
+// apiKey: "AIzaSyDTkXjNDPdbOeo32CaCkglIy-6nzuszEJM",
+//   authDomain: "orion-investment-2b141.firebaseapp.com",
+//     projectId: "orion-investment-2b141",
+//       storageBucket: "orion-investment-2b141.appspot.com",
+//         messagingSenderId: "911866094162",
+//           appId: "1:911866094162:web:bfe2dc2323995f4585358b",
+//             measurementId: "G-12SQKBDR1S",
